67bb5cada102f91caf6bb3426e9a3a039e3a5cb0
daniels <danielsalksnis@gmail.com>
2023-02-26 04:19:33 +0200

    pievienots nvim-treesitter-context

--- lua/deimoss/packer.lua
+++ lua/deimoss/packer.lua
@@ -11,6 +11,8 @@ return require('packer').startup(function(use)
 
   use 'folke/tokyonight.nvim'
   use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+  use 'nvim-treesitter/nvim-treesitter-context'
+
   use 'theprimeagen/harpoon'
   use 'mbbill/undotree'
   use 'tpope/vim-fugitive'
